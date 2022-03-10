5202a491cfc56abb4ba6073f00080d115e34b7f7
HYUNJIN SA <56509933+tkguswls1106@users.noreply.github.com>
2022-03-10 23:19:52 +0900

    Update second_models.py
    오타 수정.

--- second_models.py
+++ second_models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):  # 게시글을 남길수있는 웹앱을 만들것. 장고 모델을 상속받는 Post class를 정의.
-    title = models.charField(max_length=30)  # 모델링한 속성들을 정의. title이라는 속성은 string형이고 최대 문자열 길이는 30이다.
+    title = models.CharField(max_length=30)  # 모델링한 속성들을 정의. title이라는 속성은 string형이고 최대 문자열 길이는 30이다.
     content = models.TextField()  # content라는 속성도 정의. TextField는 문자열 길이를 정의하지 않는 긴 문자열을 선언할때 사용한다.
 
     created_at = models.DateTimeField(auto_now_add=True)  # 이 스키마를 따르는 게시물이 생성될때, 해당 이 게시글이 쓰여진 시각이 자동으로 기록됨.
