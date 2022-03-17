635abd5d2943bcee940978ab604e312f0dc0be3b
HYUNJIN SA <56509933+tkguswls1106@users.noreply.github.com>
2022-03-17 20:17:45 +0900

    Update second_views.py

--- second_views.py
+++ second_views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from second.models import Post
+from .forms import PostForm  # views.py와 같은 second 폴더안의 forms.py 파일을 불러오므로 .forms 로 적고, forms.py 안의 PostForm 클래스를 불러온다.
+
 
 # Create your views here.
 def list(request):
@@ -7,4 +9,8 @@ def list(request):
         'items': Post.objects.all()
     }
     return render(request, 'second/list.html', context)
-    
+
+
+def create(request):
+    form = PostForm()
+    return render(request, 'second/create.html', {'form':form})
