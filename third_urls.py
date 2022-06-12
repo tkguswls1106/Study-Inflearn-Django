ce389b8c3c6f54f0e5621a91d26787c193d42e0c
HYUNJIN SA <56509933+tkguswls1106@users.noreply.github.com>
2022-06-13 01:18:53 +0900

    Update third_urls.py

--- third_urls.py
+++ third_urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.list, name="list"),  # 레스토랑의 리스트를 보여주는 화면
+    path('list/', views.list, name="list"),  # 레스토랑의 리스트를 보여주는 화면이다. /third/list/ 접속 경로를 의미한다.
+    path('create/', views.create, name='restaurant-create'),
 ]
