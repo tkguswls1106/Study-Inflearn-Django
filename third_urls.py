14dba36c36c9cddb3425ad12d0771e0f23ca6f85
HYUNJIN SA <56509933+tkguswls1106@users.noreply.github.com>
2022-07-01 05:08:10 +0900

    Update third_urls.py

--- third_urls.py
+++ third_urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('list/', views.list, name="list"),  # 레스토랑의 리스트를 보여주는 화면이다. /third/list/ 접속 경로를 의미한다.
     path('create/', views.create, name='restaurant-create'),
     path('update/', views.update, name='restaurant-update'),
-    path('detail/', views.detail, name='restaurant-detail'),
     path('delete/', views.delete, name='restaurant-delete'),
+    # path('detail/', views.detail, name='restaurant-detail'),
+    path('restaurant/<int:id>/', views.detail, name='restaurant-detail'),  # detail 의 주소를 패스파라미터(쿼리파라미터 말고)를 사용할 수 있도록 재정의함.
+    path('restaurant/<int:restaurant_id>/review/create/', views.review_create, name='review-create'),
 ]
